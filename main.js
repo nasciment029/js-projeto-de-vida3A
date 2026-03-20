b3740a6c9b18ff42243de2e5f7528f2f2ba17c67
nasciment029 <flavia.maria.nascimento@escola.pr.gov.br>
2026-03-20 11:39:03 +0000

    lenght

--- main.js
+++ main.js
@@ -1,2 +1,12 @@
 const botoes = document.querySelectorAll(".botao");
+
+for(let i = 0; i < botoes.length; i = i + 1){
+    botoes[i].onclick = function(){
+        For(let j = 0; j < botoes.length; j = j +1){
+            botoes[j].classList.remove("ativo");
+        }
+        botoes[i].classList.add("ativo");
+    }
+
+}
 console.log(botoes);
